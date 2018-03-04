6a1de4eee2f93cae3085d040316896f3e3520c05
Fazzani <tunisienheni@outlook.com>
2018-03-04 09:39:23 +0100

    fix scripts deployment

--- rundown.sh
+++ rundown.sh
@@ -1,31 +1,22 @@
-#!/usr/bin/env bash
-
-set -o errexit
-trap 'echo "Aborting due to errexit on line $LINENO. Exit code: $?" >&2' ERR
-set -o errtrace
-set -o nounset
+#!/bin/bash
 
 ### ### ### ### ### ### ### ### ### ### ###
 # Functions
 ### ### ### ### ### ### ### ### ### ### ###
 
-echo; echo "Remove stacks ..."
+echo "Remove stacks ..."
+set -euox
+
 docker stack rm elk || true; echo; sleep 1;
-
 docker stack rm rabbitmq || true; echo; sleep 1;
-
 docker stack rm lb || true; echo; sleep 1;
-
 docker stack rm mariadb || true; echo; sleep 1;
 
-#docker stack rm proxpress || true; echo; sleep 1;
+echo "Remove network ..."
+docker network rm ntw_front
 
-#echo; echo "Remove network ..."
-#docker network rm ntw_front
+echo "Clean up ..."
+docker system prune -f
+docker system volume -f
 
-#echo; echo "Clean up ..."
-#docker system prune -f
-
-# by Pascal Andy | # https://twitter.com/askpascalandy
-# https://github.com/pascalandy/docker-stack-this
-#
+exit 0
