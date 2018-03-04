9fa4edd0dd74967abfaadc2b48aa89e9b00d59c1
Fazzani <tunisienheni@outlook.com>
2018-03-04 18:37:54 +0100

    fix

--- deploy-travis/deploy.sh
+++ deploy-travis/deploy.sh
@@ -1,13 +1,4 @@
 #!/bin/bash
 
 set +e 
-tar -czf package.tgz $TRAVIS_BUILD_DIR
-exitcode=$?
-
-if [ "$exitcode" != "1" ] && [ "$exitcode" != "0" ]; then
-    exit $exitcode
-fi
-set -euox
-
-ls
-scp -o "StrictHostKeyChecking no" -r $TRAVIS_BUILD_DIR package.tgz $USER@$SERVER_IP:/home/dockeradmin/synker-docker
+scp -o "StrictHostKeyChecking no" -r $TRAVIS_BUILD_DIR $USER@$SERVER_IP:/home/dockeradmin
