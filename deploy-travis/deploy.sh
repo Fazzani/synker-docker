a8b1558d793be99fa5d93f81074947f784442d38
Fazzani <tunisienheni@outlook.com>
2018-03-04 19:39:32 +0100

    fix travis

--- deploy-travis/deploy.sh
+++ deploy-travis/deploy.sh
@@ -5,7 +5,7 @@ echo "Remove deployed script"
 ssh -o "StrictHostKeyChecking no" $REMOTE_USER@$REMOTE_HOST "rm -R /home/$REMOTE_USER/synker-docker" || true
 
 echo "Copy scripts to remote host"
-scp -o "StrictHostKeyChecking no" -r $TRAVIS_BUILD_DIR $REMOTE_USER@$REMOTE_HOST:/home/$USER
+scp -o "StrictHostKeyChecking no" -r $TRAVIS_BUILD_DIR $REMOTE_USER@$REMOTE_HOST:/home/$REMOTE_USER
 
 sleep 1
 
