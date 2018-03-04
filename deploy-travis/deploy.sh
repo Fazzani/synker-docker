56cc2570e93bc9f663ddb5b337fd8ba7bdff69fd
Fazzani <tunisienheni@outlook.com>
2018-03-04 18:26:28 +0100

    fix

---
+++ deploy-travis/deploy.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+set +e 
+tar -czf package.tgz $TRAVIS_BUILD_DIR
+exitcode=$?
+
+if [ "$exitcode" != "1" ] && [ "$exitcode" != "0" ]; then
+    exit $exitcode
+fi
+set -euox
+
+ls
+scp package.tgz $USER@$SERVER_IP:/home/dockeradmin/synker-docker/package.tgz
