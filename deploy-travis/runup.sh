5185528bb79796beda1fa0865d52513e4629a441
Fazzani <tunisienheni@outlook.com>
2018-03-08 23:43:51 +0100

    fix deploy

--- deploy-travis/runup.sh
+++ deploy-travis/runup.sh
@@ -28,8 +28,8 @@ echo $MYSQL_ROOT_PASSWORD > mysql_password.txt
 
 export $(cat .env)
 
-docker stack deploy -c elk-stack.yml elk
 docker stack deploy -c traefik-consul-stack.yml lb
+docker stack deploy -c elk-stack.yml elk
 docker stack deploy -c mariadb-stack.yml mariadb
 docker stack deploy -c rabbitmq-stack.yml rabbit
 docker stack deploy -c ./webgrab/docker-compose.yml webgrab
