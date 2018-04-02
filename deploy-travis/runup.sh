5fe4588eba4290c0d753ef1cad7c07f0780e046f
Fazzani <tunisienheni@outlook.com>
2018-04-02 18:56:29 +0200

    fix

--- deploy-travis/runup.sh
+++ deploy-travis/runup.sh
@@ -13,11 +13,11 @@ set -euox
 
 cd /home/$REMOTE_USER/synker-docker/
 
-sudo docker network create --driver overlay ntw_front \
+docker network create --driver overlay ntw_front \
   --attachable || true \
   --opt encrypted=true || true
 
-sudo docker network create --driver overlay ingress_net_backend \
+docker network create --driver overlay ingress_net_backend \
   --attachable || true \
   --subnet=70.28.0.0/16 \
   --opt com.docker.network.driver.mtu=9216 \
@@ -26,16 +26,16 @@ sudo docker network create --driver overlay ingress_net_backend \
 echo $MYSQL_PASSWORD > mysql_root_password.txt
 echo $MYSQL_ROOT_PASSWORD > mysql_password.txt
 
-sudo export $(cat .env)
+export $(cat .env)
 
-sudo docker stack deploy -c traefik-consul-stack.yml lb
+docker stack deploy -c traefik-consul-stack.yml lb
 sleep 20
-sudo docker stack deploy -c elk-stack.yml elk
-sudo docker stack deploy -c rabbitmq-stack.yml rabbit
-sudo docker stack deploy -c ./webgrab/docker-compose.yml webgrab
-sudo docker stack deploy -c synker-stack.yml synker
+docker stack deploy -c elk-stack.yml elk
+docker stack deploy -c rabbitmq-stack.yml rabbit
+docker stack deploy -c ./webgrab/docker-compose.yml webgrab
+docker stack deploy -c synker-stack.yml synker
 
 echo "Clean up ..."
-sudo docker system prune -f
+docker system prune -f
 
 exit 0
