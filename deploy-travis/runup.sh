89f221490fb3ec5ce33bec1f09bc8a9a8f8721ef
Fazzani <tunisienheni@outlook.com>
2018-04-08 01:39:02 +0200

    fix

--- deploy-travis/runup.sh
+++ deploy-travis/runup.sh
@@ -56,7 +56,7 @@ docker network create --driver overlay ingress_net_backend \
 
 echo $MYSQL_PASSWORD > mysql_password.txt
 echo $MYSQL_ROOT_PASSWORD > mysql_root_password.txt
-
+echo -e $(cat .env)
 export $(echo -e $(cat .env))
 
 docker stack deploy -c traefik-consul-stack.yml lb
