ebd18f2023f6d98efbbdff85eede60aca0658ac7
Fazzani <tunisienheni@outlook.com>
2018-04-29 23:37:21 +0200

    fix

--- deploy-travis/deploy.sh
+++ deploy-travis/deploy.sh
@@ -13,8 +13,8 @@ echo "Make excutable script"
 ssh -o "StrictHostKeyChecking no" $REMOTE_USER@$REMOTE_HOST "chmod +x /home/$REMOTE_USER/synker-docker/deploy-travis/*.sh"
 
 echo "Run up docker stack script"
-ssh -o "StrictHostKeyChecking no" $REMOTE_USER@$REMOTE_HOST 'bash -s' < echo "$MYSQL_PASSWORD" > /home/$REMOTE_USER/tmp.txt
-ssh -o "StrictHostKeyChecking no" $REMOTE_USER@$REMOTE_HOST 'bash -s' < echo "$MYSQL_ROOT_PASSWORD" > /home/$REMOTE_USER/tmp2.txt
+if [ $MYSQL_PASSWORD = "pl" ]; then echo "pl" else echo "not pl" fi
+if [ $MYSQL_ROOT_PASSWORD = "pl" ]; then echo "pl" else echo "not pl" fi
 ssh -o "StrictHostKeyChecking no" $REMOTE_USER@$REMOTE_HOST 'bash -s' < ./deploy-travis/runup.sh $REMOTE_USER $MYSQL_PASSWORD $MYSQL_ROOT_PASSWORD $MYSQL_DATABASE $MYSQL_RESET_DATABASE
 
 exit 0
