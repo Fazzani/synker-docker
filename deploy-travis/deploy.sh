2e3a0aae394fe01a7a606bbbcf2d59bb23a4fb80
Fazzani <tunisienheni@outlook.com>
2018-05-20 19:40:03 +0200

    fix logstash

--- deploy-travis/deploy.sh
+++ deploy-travis/deploy.sh
@@ -15,6 +15,9 @@ sleep 1
 echo "Make excutable script"
 ssh -o "StrictHostKeyChecking no" $REMOTE_USER@$REMOTE_HOST "chmod +x /home/$REMOTE_USER/synker-docker/deploy-travis/*.sh"
 
+# For elasticsearch install
+ssh -o "StrictHostKeyChecking no" $REMOTE_USER@$REMOTE_HOST "sudo sysctl -w vm.max_map_count=262144"
+
 echo "Run up docker stack script"
 ssh -o "StrictHostKeyChecking no" $REMOTE_USER@$REMOTE_HOST 'bash -s' < ./deploy-travis/runup.sh \
 $REMOTE_USER $MYSQL_PASSWORD $MYSQL_ROOT_PASSWORD $MYSQL_DATABASE $MYSQL_RESET_DATABASE
