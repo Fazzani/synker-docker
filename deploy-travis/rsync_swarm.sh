c6251609efe9caebd45596a77744728143c7377e
Fazzani <tunisienheni@outlook.com>
2018-05-21 11:10:26 +0200

    fix rsync swarm script

--- deploy-travis/rsync_swarm.sh
+++ deploy-travis/rsync_swarm.sh
@@ -9,16 +9,16 @@
 NOW=$(date +"%d-%m-%Y")
 echo "[$NOW] rsync batch started"
 
-dest="/home/pi/toshiba/backup/swarm"
-src_host="ansible@ovh1"
-src_host_alias="151.80.235.155 ovh1"
-cron="0 6 * * 0,3"
+dest="/home/pi/toshiba/backup/swarm";
+src_host="ansible@ovh1";
+src_host_alias="151.80.235.155 ovh1";
+cron="0 6 * * 0,3";
 
 exclude_swarm_terms_array=('*.bak' '*.log' '/mnt/nfs/freebox' '/mnt/nfs/webgrab')
 exclude_swarm_terms=$(printf "%s\n" "${exclude_swarm_terms_array[@]}")
 
 [ -d $dest ] || mkdir -p ${dest}
-sudo sh -c '(src_host_alias="151.80.235.155 ovh1"; cat /etc/hosts | grep -v -F "${src_host_alias}" ; echo "${src_host_alias}") > /etc/hosts'
+(src_host_alias="151.80.235.155 ovh1"; cat /etc/hosts | grep -v -F "${src_host_alias}"; echo "${src_host_alias}") | sudo tee /etc/hosts > /dev/null
 ssh-copy-id "${src_host}"
 echo -e "${exclude_swarm_terms}" > "${dest}/exclude_swarm.txt"
 
