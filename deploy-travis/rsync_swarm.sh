637a75be05aa43370d76025dd95cc5fd6ba7b7b1
Fazzani <tunisienheni@outlook.com>
2018-05-22 22:40:07 +0200

    fix

--- deploy-travis/rsync_swarm.sh
+++ deploy-travis/rsync_swarm.sh
@@ -19,7 +19,7 @@ src_host="ansible@ovh1";
 src_host_alias="151.80.235.155 ovh1";
 cron="0 6 * * 0,3";
 
-exclude_swarm_terms_array=('*.bak' '*.log' '/mnt/nfs/freebox' '/mnt/nfs/webgrab','/mnt/nfs/emby/config/cache','/mnt/nfs/emby/config/siteini.pack')
+exclude_swarm_terms_array=('*.bak' '*.log' '/mnt/nfs/freebox' '/mnt/nfs/webgrab' '/mnt/nfs/emby/config/cache' '/mnt/nfs/emby/config/siteini.pack')
 exclude_swarm_terms=$(printf "%s\n" "${exclude_swarm_terms_array[@]}")
 
 [ -d $dest ] || mkdir -p ${dest}
@@ -27,7 +27,7 @@ exclude_swarm_terms=$(printf "%s\n" "${exclude_swarm_terms_array[@]}")
 ssh-copy-id "${src_host}"
 echo -e "${exclude_swarm_terms}" > "${dest}/exclude_swarm.txt"
 
-croncmd="rsync -avz -e ssh --stats --progress --exclude-from="${dest}/exclude_swarm.txt" "${src_host}:/mnt/nfs" ${dest} > /log/rsync_swarm.log 2>&1"
+croncmd="rsync -avz -e ssh --stats --progress --exclude-from="${dest}/exclude_swarm.txt" "${src_host}:/mnt/nfs" ${dest} | sudo tee /log/rsync_swarm.log 2>&1"
 cronjob="${cron} ${croncmd}"
 ( crontab -l | grep -v -F "$croncmd" ; echo -e "#rsync swarm \n${cronjob}" ) | crontab -
 
