10010166114794325c6cdbaab13a8f9c37c5ec90
Fazzani <tunisienheni@outlook.com>
2018-06-02 13:11:06 +0200

    fix postgres password

--- deploy-travis/runup.sh
+++ deploy-travis/runup.sh
@@ -98,8 +98,7 @@ sudo docker network create --driver overlay ingress_net_backend \
   --opt com.docker.network.driver.mtu=9216 \
   --opt encrypted=true || true
 
-echo $MYSQL_PASSWORD > mysql_password.txt
-echo $MYSQL_ROOT_PASSWORD > mysql_root_password.txt
+echo $POSTGRES_PASSWORD > postgres_password.txt
 awk '{ sub("\r$", ""); print }' .env > env
 export $(cat env)
 echo $TAG
