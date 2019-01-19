c0aaa3bc0c6075bc4b9e7d7dad9713864dcb7234
Heni Fazzani <heni.fazzani@seloger.com>
2019-01-19 16:12:19 +0100

    fix

--- script-folder/mongo_setup.sh
+++ script-folder/mongo_setup.sh
@@ -7,3 +7,13 @@ mongo ${MONGO_INITDB_DATABASE:-synkerdb} \
         -p ${MONGO_INITDB_ROOT_PASSWORD:-password} \
         --authenticationDatabase admin \
         --eval "db.createUser({user: '${DATABASE_USERNAME:-pl}', pwd: '${DATABASE_PASSWORD:-password}', roles:[{role:'dbOwner', db: '${MONGO_INITDB_DATABASE:-synkerdb}'}]});"
+
+echo 'Config Admin User'
+
+mongo ${MONGO_INITDB_DATABASE:-admin} \
+        -u ${MONGO_INITDB_ROOT_USERNAME:-admin} \
+        -p ${MONGO_INITDB_ROOT_PASSWORD:-password} \
+        --authenticationDatabase admin \
+        --eval "db.createUser( { user: "admin", pwd: "password", roles: [ { role: "root", db: "admin" }]});"
+
+exit 0
