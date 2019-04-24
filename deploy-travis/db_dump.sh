09f3aad209d74d49cdc74b11fe1985c42f3244aa
Heni Fazzani <heni.fazzani@seloger.com>
2019-04-24 21:59:31 +0200

    fix

--- deploy-travis/db_dump.sh
+++ deploy-travis/db_dump.sh
@@ -1,34 +1,34 @@
 #!/bin/bash
 
-#######################################
+#-------------------------------------
+#
 # Dumping Postgresql database
 # with max retention of dump files
-#######################################
+#
+#-------------------------------------
 
-# script inputs
+####################################### script inputs
 
 db_user=${1:-pl}
 database=${2:-playlist}
 retention=${3:-5}
-
-# functions
+####################################### functions
 
 function purge() {
-    files_pattern_to_delete=${1:'*.gz'}
-    retentionMax={$2:-3}
+    files_pattern_to_delete=${1:-*.gz}
+    retentionMax=${2:-3}
     current_dir=${3:-.}
 
     files_count_to_delete="$(($(find $current_dir -name $files_pattern_to_delete -type f | wc -l) - $r))"
     rm -rf $(find $current_dir -name $files_pattern_to_delete -type f | sort -n | head -n $files_count_to_delete)
 }
-
-# Variables
+###################################### Variables
 
 dump_dir="/mnt/nfs/postgres/data/"
 dump_filename="dump_${database}_$(date +%F).sql"
 local_dump_file_path="${dump_dir}${dump_filename}"
 
-# script body
+##################################### script body
 
 sudo docker exec -i $(sudo docker ps -aq -f "name=synker_synkerdb") \
      /bin/bash -c "pg_dump -U $db_user $database > /var/lib/postgresql/data/${dump_filename}"
