5fb04589a0bfe85544952e7924f00c667be458a5
Heni Fazzani <heni.fazzani@seloger.com>
2019-04-24 21:12:30 +0200

    fix retention database

--- deploy-travis/db_dump.sh
+++ deploy-travis/db_dump.sh
@@ -4,8 +4,8 @@ function purge() {
     files_pattern_to_delete=${1:*.gz}
     retentionMax={$2:-3}
     current_dir=${3:-.}
-    
-    files_count_to_delete="$(($(find /mnt/nfs/postgres/data/ -name *.gz -type f | wc -l)-$r))"
+
+    files_count_to_delete="$(($(find $current_dir -name $files_pattern_to_delete -type f | wc -l) - $r))"
     rm -rf $(find $current_dir -name $files_pattern_to_delete -type f | sort -n | head -n $files_count_to_delete)
 }
 
@@ -16,10 +16,10 @@ database=${2:-playlist}
 retention=5
 dump_dir="/mnt/nfs/postgres/data/"
 dump_filename="dump_${database}_$(date +%F).sql"
-local_dump_file_path="/mnt/nfs/postgres/data/$dump_filename"
+local_dump_file_path="${dump_dir}${dump_filename}"
 
 sudo docker exec -i $(sudo docker ps -aq -f "name=synker_synkerdb") \
-/bin/bash -c "pg_dump -U $db_user $database > ${dump_dir}${dump_filename}"
+/bin/bash -c "pg_dump -U $db_user $database > /var/lib/postgresql/data/${dump_filename}"
 
 [ -f $local_dump_file_path ] || exit -1
 
@@ -27,6 +27,6 @@ echo "Compressiong dump"
 gzip -f $local_dump_file_path
 
 echo "Purging dumps (keeping only last $retention dump files)""
-purge "*.gz" $retention $dump_dir
+purge "dump_${database}_*.gz" $retention $dump_dir
 
 exit 0
